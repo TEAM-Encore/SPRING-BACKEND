ae67d3ece3798b05e8c46f417c9a0e5f4d05c001
yumzen <yeom.ji.eun35@gmail.com>
2024-11-05 13:26:45 +0900

    #1 Chore: 공통 Response 세팅

---
+++ src/main/java/encore/server/global/exception/ApplicationException.java
@@ -0,0 +1,10 @@
+package encore.server.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApplicationException extends RuntimeException {
+    public ErrorCode errorCode;
+}
