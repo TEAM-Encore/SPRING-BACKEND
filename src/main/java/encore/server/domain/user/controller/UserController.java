39c5b44e6fc78d5aee49457d2aead90c0d22e8ca
kangchanwook <kan0202@naver.com>
2024-12-07 17:08:02 +0900

    #44 Feat: 테스트를 위한 login, signup Controller 구현

--- src/main/java/encore/server/domain/user/controller/UserController.java
+++ src/main/java/encore/server/domain/user/controller/UserController.java
@@ -1,8 +1,17 @@
 package encore.server.domain.user.controller;
 
+import encore.server.domain.user.dto.request.UserLoginReq;
+import encore.server.domain.user.dto.request.UserSignupReq;
+import encore.server.domain.user.service.UserService;
+import encore.server.global.common.ApplicationResponse;
+import encore.server.global.exception.ApplicationException;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/user")
 @Tag(name = "User", description = "사용자 API")
 public class UserController {
+
+    private final UserService userService;
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ApplicationResponse<String> signup(@RequestBody @Valid UserSignupReq userSignupReq) {
+        return ApplicationResponse.ok(userService.signup(userSignupReq));
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public ApplicationResponse<String> login(@RequestBody @Valid UserLoginReq userLoginReq) {
+        return ApplicationResponse.ok(userService.login(userLoginReq));
+    }
+
 }
