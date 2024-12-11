0beda3bfe4abad1135b72b182163a0a7a251a162
raours <ryk2003@naver.com>
2024-12-12 01:04:45 +0900

    #40 Fix: 장소, 회차 반환하도록 수정,dto변환 converter단으로 이동

--- src/main/java/encore/server/domain/musical/dto/response/MusicalRes.java
+++ src/main/java/encore/server/domain/musical/dto/response/MusicalRes.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MusicalRes(
         Long musicalId,
         String title,
-        Long series
+        Long series,
+        String location,
+        List<String> showTimes
 ) {
 }
