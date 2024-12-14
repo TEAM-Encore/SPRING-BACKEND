913afa1aff70c689edccfb95071f3c1d3adc55ac
raours <ryk2003@naver.com>
2024-12-14 21:09:50 +0900

    #40 Fix: 배우검색api builder, converter사용하도록 변경

--- src/main/java/encore/server/domain/ticket/converter/TicketConverter.java
+++ src/main/java/encore/server/domain/ticket/converter/TicketConverter.java
@@ -11,34 +11,12 @@ import java.util.stream.Collectors;
 
 public class TicketConverter {
 
-    // Ticket -> TicketCreateRes 변환
-    public static TicketCreateRes toCreateResponse(Ticket ticket) {
-        return TicketCreateRes.builder()
-                .id(ticket.getId())
-                .musicalId(ticket.getMusical().getId())
-                .viewedDate(ticket.getViewedDate())
-                .showTime(ticket.getShowTime())
-                .seat(ticket.getSeat())
-                .actors(ticket.getActors().stream()
-                        .map(actor -> new ActorDTO(actor.getId(), actor.getName(), actor.getActorImageUrl()))
-                        .collect(Collectors.toList()))
-                .ticketImageUrl(ticket.getTicketImageUrl())
+    public static ActorDTO toActorDTO(Actor actor) {
+        return ActorDTO.builder()
+                .id(actor.getId())
+                .name(actor.getName())
+                .actorImageUrl(actor.getActorImageUrl())
                 .build();
     }
 
-    // Ticket -> TicketRes 변환
-    public static TicketRes toResponse(Ticket ticket) {
-        return new TicketRes(ticket);
-    }
-
-    // ActorDTO -> Actor 변환
-    public static List<Actor> toActorList(List<ActorDTO> actorDTOs) {
-        return actorDTOs.stream()
-                .map(dto -> Actor.builder()
-                        .id(dto.id())
-                        .name(dto.name())
-                        .actorImageUrl(dto.actorImageUrl())
-                        .build())
-                .collect(Collectors.toList());
-    }
 }
