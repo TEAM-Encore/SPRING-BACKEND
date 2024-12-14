fa94bb8df6467cf5446dd7a9930f45670ebebe84
raours <ryk2003@naver.com>
2024-12-14 18:15:52 +0900

    #40 Feat: 티켓북 기간별 조회api 생성

--- src/main/java/encore/server/domain/ticket/dto/response/TicketRes.java
+++ src/main/java/encore/server/domain/ticket/dto/response/TicketRes.java
@@ -1,4 +1,33 @@
 package encore.server.domain.ticket.dto.response;
 
-public record TicketRes() {
+import encore.server.domain.ticket.entity.Ticket;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+public record TicketRes(
+        Long id,
+        String musicalTitle,
+        Long series,
+        LocalDate viewedDate,
+        String venue,
+        String seat,
+        List<String> actors
+) {
+    public TicketRes(Ticket ticket) {
+        this(
+                ticket.getId(),
+                ticket.getMusical().getTitle(),
+                ticket.getMusical().getSeries(),
+                ticket.getViewedDate(),
+                ticket.getMusical().getLocation(),
+                ticket.getSeat(),
+                ticket.getActors().stream()
+                        .map(actor -> actor.getName())
+                        .collect(Collectors.toList())
+        );
+    }
 }
