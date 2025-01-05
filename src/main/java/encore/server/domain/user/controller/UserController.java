ee825a5a91f6f11566f87bd4490a5f0acdd725d7
kangchanwook <kan0202@naver.com>
2025-01-05 15:01:54 +0900

    #69 Feat: 유저 닉네임 검증 API 컨트롤러 단 구현

--- src/main/java/encore/server/domain/user/controller/UserController.java
+++ src/main/java/encore/server/domain/user/controller/UserController.java
@@ -3,16 +3,21 @@ package encore.server.domain.user.controller;
 import encore.server.domain.user.dto.request.UserLoginReq;
 import encore.server.domain.user.dto.request.UserSignupReq;
 import encore.server.domain.user.dto.response.UserLoginRes;
+import encore.server.domain.user.dto.response.UserNicknameValidationRes;
 import encore.server.domain.user.dto.response.UserSignupRes;
 import encore.server.domain.user.service.UserAuthService;
+import encore.server.domain.user.service.UserSetupService;
 import encore.server.global.common.ApplicationResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserAuthService userAuthService;
-
+    private final UserSetupService userSetupService;
     // 회원가입
     @PostMapping("/signup")
     @Operation(summary = "회원가입", description = "주어진 정보로 회원가입을 진행합니다.")
@@ -35,6 +40,12 @@ public class UserController {
     @Operation(summary = "로그인", description = "주어진 정보로 로그인을 진행합니다.")
     public ApplicationResponse<UserLoginRes> login(@RequestBody @Valid UserLoginReq userLoginReq) {
         return ApplicationResponse.ok(userAuthService.login(userLoginReq));
+    }
+
+    @GetMapping("/nickname-validation/{nickname}")
+    @Operation(summary = "유저 닉네임 검증", description = "주어진 파라미터로 중복확인과 제약조건 검증을 진행합니다.")
+    public ApplicationResponse<UserNicknameValidationRes> validateUserNickname(@PathVariable String nickname) {
+        return ApplicationResponse.ok(userSetupService.validateUserNickname(nickname));
     }
 
 }
