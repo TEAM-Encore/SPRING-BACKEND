ba7f9e53af00f45e4b5bdee6203f584f69a95323
chanwookK <kan0202@naver.com>
2025-07-15 02:04:53 +0900

    #44 Feat: Kakao OAuth 로그인 구현

---
+++ src/main/java/encore/server/domain/user/dto/profile/KakaoProfileResponse.java
@@ -0,0 +1,20 @@
+package encore.server.domain.user.dto.profile;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class KakaoProfileResponse {
+
+  private Long id;
+  private String connected_at;
+  private KakaoAccount kakao_account;
+
+  @Data
+  @NoArgsConstructor
+  public static class KakaoAccount {
+    private String email;
+    private String name;
+  }
+}
