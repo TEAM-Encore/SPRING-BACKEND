ea4f41170712c82ef62dbb04ead7d613043271a5
chanwookK <kan0202@naver.com>
2025-12-23 23:15:28 +0900

    Fix: 토큰 필터 오류 수정 (OAuth 경로 추가)

--- src/main/java/encore/server/domain/user/controller/OAuthController.java
+++ src/main/java/encore/server/domain/user/controller/OAuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/oauth2")
+@RequestMapping("/api/v1/oauth2")
 @RequiredArgsConstructor
 public class OAuthController {
 
