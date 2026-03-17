b8d4c8f842b31ae404f35957235219e64c6ed8fd
chanwookK <kan0202@naver.com>
2026-03-17 18:07:15 +0900

    Fix: h2 콘솔 관련 코드 제거

--- src/main/java/encore/server/global/config/SecurityConfig.java
+++ src/main/java/encore/server/global/config/SecurityConfig.java
@@ -29,9 +29,7 @@ public class SecurityConfig{
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers.frameOptions().sameOrigin()) // ✅ H2 콘솔 허용
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/h2-console/**").permitAll() // ✅ 콘솔 접근 허용
                         .requestMatchers("/**").permitAll()
                 )
                 .httpBasic(Customizer.withDefaults())
